6ffde6d4a863f604dec77631fc52c36219ba9328
cjn42 <c.jullien@hotmail.fr>
2015-07-03 13:42:35 +0200

    without holidays

--- feature_extractor.py
+++ feature_extractor.py
@@ -13,18 +13,18 @@ class FeatureExtractor(object):
         X_encoded = X_df
         
         #uncomment the line below in the submission
-        path = os.path.dirname(__file__)
+        #path = os.path.dirname(__file__)
         X_encoded = X_df
         X_encoded = X_encoded.join(pd.get_dummies(X_encoded['Departure'], prefix='d'))
         X_encoded = X_encoded.join(pd.get_dummies(X_encoded['Arrival'], prefix='a'))
         X_encoded = X_encoded.drop('Departure', axis=1)
         X_encoded = X_encoded.drop('Arrival', axis=1)
     
-        data_holidays = pd.read_csv(os.path.join(path, "data_holidays.csv"))
-        X_holidays = data_holidays[['DateOfDeparture','Xmas','Xmas-1','NYD','NYD-1','Ind','Thg','Thg+1']]
-        X_encoded = X_encoded.set_index(['DateOfDeparture'])
-        X_holidays = X_holidays.set_index(['DateOfDeparture'])
-        X_encoded = X_encoded.join(X_holidays).reset_index()        
+     #   data_holidays = pd.read_csv(os.path.join(path, "data_holidays.csv"))
+     #   X_holidays = data_holidays[['DateOfDeparture','Xmas','Xmas-1','NYD','NYD-1','Ind','Thg','Thg+1']]
+     #   X_encoded = X_encoded.set_index(['DateOfDeparture'])
+     #   X_holidays = X_holidays.set_index(['DateOfDeparture'])
+     #   X_encoded = X_encoded.join(X_holidays).reset_index()        
         
         X_encoded['DateOfDeparture'] = pd.to_datetime(X_encoded['DateOfDeparture'])
         X_encoded['year'] = X_encoded['DateOfDeparture'].dt.year
