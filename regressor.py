6a50375c1bc74abd0b8a7bfa41c8b893ab3d67dc
cjn42 <c.jullien@hotmail.fr>
2015-07-04 15:52:09 +0200

    tada

--- regressor.py
+++ regressor.py
@@ -1,9 +1,9 @@
-from sklearn.ensemble import GradientBoostingRegressor
+from sklearn.linear_model import LinearRegression
 from sklearn.base import BaseEstimator
 
 class Regressor(BaseEstimator):
     def __init__(self):
-        self.clf = GradientBoostingRegressor( n_estimators = 1950 , max_depth = 9 , max_features = 27)
+        self.clf = LinearRegression()
         
     def fit(self, X, y):
         self.clf.fit(X, y)
