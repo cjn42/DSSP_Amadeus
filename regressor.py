44787daec0831ad663517db74e29c0ce91c03ff8
cjn42 <c.jullien@hotmail.fr>
2015-07-07 13:52:08 +0200

    Revert "final"
    This reverts commit 841842684ddbbacc062ef06b36fa6251546b2282.

--- regressor.py
+++ regressor.py
@@ -1,17 +1,12 @@
-from sklearn.ensemble import RandomForestRegressor
-from sklearn.ensemble import AdaBoostRegressor
-from sklearn.ensemble import GradientBoostingRegressor
+from sklearn.linear_model import LinearRegression
 from sklearn.base import BaseEstimator
-
 
 class Regressor(BaseEstimator):
     def __init__(self):
-        self.clf1 = GradientBoostingRegressor( n_estimators = 1950 , max_depth = 9 , max_features = 27)
-        self.clf2 = AdaBoostRegressor(RandomForestRegressor(n_estimators=50, max_depth=50, max_features=25), n_estimators=20)
-                
-    def fit(self, X, y):
-        self.clf1.fit(X, y)
-        self.clf2.fit(X, y)
+        self.clf = LinearRegression()
         
+    def fit(self, X, y):
+        self.clf.fit(X, y)
+
     def predict(self, X):
-        return self.clf1.predict(X) * 0.9 + self.clf2.predict(X) * 0.1
+        return self.clf.predict(X)
